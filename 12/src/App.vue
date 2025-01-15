30463031095314c7f99ab557416a186c6481533f
Patrycja Kalita <pati.kalita@gmail.com>
2025-01-15 18:48:09 +0100

    Twelfth small project - part 4

--- 12/src/App.vue
+++ 12/src/App.vue
@@ -15,6 +15,13 @@
       <p>{{ againAnyName.nextProp }}</p>
       <!-- </template> -->
     </CourseGoals>
+
+    <!-- Dynamic Components -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
@@ -23,6 +30,8 @@ import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
 import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
 
 export default {
   components: {
@@ -31,15 +40,23 @@ export default {
     "user-info": UserInfo,
     "badge-list": BadgeList,
     CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Patrycja Kalita",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>

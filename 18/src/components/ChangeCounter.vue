f953983407189d183efbf82cff8192cb1897226a
Patrycja Kalita <pati.kalita@gmail.com>
2025-02-17 17:49:21 +0100

    Eighteenth small project - part 6

--- 18/src/components/ChangeCounter.vue
+++ 18/src/components/ChangeCounter.vue
@@ -1,13 +1,18 @@
 <template>
-  <button @click="add">+ 2</button>
+  <!-- <button @click="add">+ 2</button> -->
+  <button @click="increment">+ 2</button>
+  <button @click="increase({ value: 2 })">+ 4</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   methods: {
-    add() {
-      this.$store.dispatch("increment");
-    },
+    // add() {
+    //   this.$store.dispatch("increment");
+    // },
+    ...mapActions(["increment", "increase"]),
   },
 };
 </script>
--- 18/src/main.js
+++ 18/src/main.js
@@ -42,6 +42,7 @@ const store = createStore({
       }, 2000);
     },
     increase(context, payload) {
+      // console.log(context);
       context.commit("increase", payload);
     },
   },

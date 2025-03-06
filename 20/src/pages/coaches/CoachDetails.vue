9fd65ccefa68ee3df0de67abf480f4b78151f528
Patrycja Kalita <pati.kalita@gmail.com>
2025-03-06 20:02:00 +0100

    Twentieth small project - part 4

--- 20/src/pages/coaches/CoachDetails.vue
+++ 20/src/pages/coaches/CoachDetails.vue
@@ -1,3 +1,6 @@
 <template>
   <h3>Coach Details</h3>
+
+  <router-view></router-view>
+  <router-link to="/coaches/c1/contact">Contact</router-link>
 </template>
--- 20/src/pages/coaches/CoachesList.vue
+++ 20/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,14 @@
 <template>
   <section>Filter</section>
 
-  <section>List</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+
+    <ul>
+      List
+    </ul>
+  </section>
 </template>
